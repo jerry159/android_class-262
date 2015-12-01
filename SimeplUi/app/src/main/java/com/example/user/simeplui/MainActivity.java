5c57c0536d70dff9587182fe2c9ddad8976f574b
jerry159 <s893042@hotmail.com>
2015-12-01 14:40:14 +0800

    資料儲存的範例
    1.使用SharedPreferences使用
    2.將輸入的字串寫入倒檔案裡面(FileOutputStream)
    3.檔案讀取動作(FileInputStream)
    4.顯示出來
    PS: 加強注解

--- SimeplUi/app/src/main/java/com/example/user/simeplui/MainActivity.java
+++ SimeplUi/app/src/main/java/com/example/user/simeplui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.simeplui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -12,23 +14,46 @@ public class MainActivity extends AppCompatActivity {
 
     private  EditText edittext ; // 建立EditText 物件
     private CheckBox hideCheckBox ; // 建立CheckBox 物件
+
+    // 用途儲存私有的簡單資料在鍵-值配對 簡單存放到的編輯
+    private SharedPreferences sharedPreferences; // 轻量级的存储类的物件
+    private SharedPreferences.Editor editor; // 用來編輯轻量级的存储类的物件
+
     // 建立生命週期
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);// 指定layout檔
-          edittext = (EditText)findViewById(R.id.input); // 物件轉換，透過findViewById方式尋找Ｒ.的ID.的名稱(ＮＡＭＥ)
-          edittext.setText("GO input any word"); // 設定指定文字
 
-          edittext.setOnKeyListener(new View.OnKeyListener() { //監聽鍵盤動作
-              @Override
-              public boolean onKey(View v, int keyCode, KeyEvent keyEvent) {
-                  if (keyEvent.getAction() == keyEvent.ACTION_DOWN) { // 監聽鍵盤動作
-                      if (keyCode == KeyEvent.KEYCODE_ENTER) {  //按鈕ENTER
-                          submit(v);
-                          return true;
-                      }
-                  }
+        sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        /** 第一個參數 放置檔名 第二個參數 檔案分享模式
+        * MODE_PRIVATE	該檔案是私有的，其它應用程式都無法存取（預設值）。
+        * MODE_APPEND	當該檔案存在時，資料會從後面繼續寫入，否則就建立新檔案。
+        * MODE_WORLD_READABLE	裝置內的其它應用程式都可以讀取。
+        * MODE_WORLD_WRITEABLE	裝置內的應用程式都可以寫入。
+        * MODE_MULTI_PROCESS	為一個旗標，在Android 2.3及以前，該旗標預設為開啟，允許多個行程同時存取同一個SharedPrecferences物件。而之後的Android版本，必須透過明確的將值設定後才能開啟多個行程的存取。
+        * */
+
+        editor = sharedPreferences.edit(); // 將原本資料提交SharedPreferences的的編輯器用
+
+        edittext = (EditText)findViewById(R.id.input); // 物件轉換，透過findViewById方式尋找Ｒ.的ID.的名稱(ＮＡＭＥ)
+
+        //edittext.setText("GO input any word"); // 設定指定文字
+
+        edittext.setText(sharedPreferences.getString("edittext", ""));//指定key從存储类載出資料
+        /** 資料的取出動作
+         *  第一個參數 取得放置key物件名稱並取出之前資料 第二參數 如果取出key物件沒有任何資料的話，必須指定預設資訊文字
+        **/
+
+        edittext.setOnKeyListener(new View.OnKeyListener() { //監聽鍵盤動作
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent keyEvent) {
+                if (keyEvent.getAction() == keyEvent.ACTION_DOWN) { // 監聽鍵盤動作
+                    if (keyCode == KeyEvent.KEYCODE_ENTER) {  //按鈕ENTER
+                        submit(v);
+                        return true;
+                    }
+                }
                   return false;
               }
           });
@@ -44,6 +69,14 @@ public class MainActivity extends AppCompatActivity {
     public void submit(View view){
 
         String test = edittext.getText().toString();
+        editor.putString("edittext", test);
+        /**
+         * 資料的存入動作
+         * 第一個參數 放置key物件名稱 第二參數 存放的資料
+         **/
+        editor.commit(); // 必須執行這行，資料才會真正存入到檔案裡面
+        Utils.writeFile(this, "history.txt", test + "\n"); // 將字串寫入到檔案動作
+
         if(hideCheckBox.isChecked()){
             test = "**********" ;
             edittext.setText("**********");
@@ -51,11 +84,119 @@ public class MainActivity extends AppCompatActivity {
             edittext.setText("");
         }
 
-        Toast.makeText(this,test,Toast.LENGTH_LONG).show(); // 顯示在螢幕上的氣泡訊息
+        // sethistry();
 
-       // Toast.makeText(this,"132132132131321321213213",Toast.LENGTH_LONG).show(); // 短暫訊息_顯示在螢幕上的氣泡訊息
+        String fileContent = Utils.readFile(this,"history.txt");// 從檔案中讀取內容
+        Toast.makeText(this, fileContent, Toast.LENGTH_LONG).show();
+        /*
+         * 參數一 指定是你的應用程式(Application)或活動(Activity)物件
+         * 參數二 需要顯示文字(帶入可以是字串或是可以用到資源庫(RES)的Values的表示ex:R.string.go1)
+         * 參數三 顯示時間長度
+         * Toast.LENGTH_LONG  長時間顯示
+         * Toast.LENGTH_SHORT 短時間顯示
+         * PS 記得要下.SHOW()才能正常顯示
+         */
+
+        //Toast.makeText(this,test,Toast.LENGTH_LONG).show(); // 長期顯示_顯示在螢幕上的氣泡訊息
+        /*
+         * 參數一 指定是你的應用程式(Application)或活動(Activity)物件
+         * 參數二 需要顯示文字(帶入可以是字串或是可以用到資源庫(RES)的Values的表示ex:R.string.go1)
+         * 參數三 顯示時間長度
+         * Toast.LENGTH_LONG  長時間顯示
+         * Toast.LENGTH_SHORT 短時間顯示
+         * PS 記得要下.SHOW()才能正常顯示
+         */
+
+        //Toast.makeText(this,"132132132131321321213213",Toast.LENGTH_LONG).show(); // 短暫訊息_顯示在螢幕上的氣泡訊息
+        /*
+         * 參數一 指定是你的應用程式(Application)或活動(Activity)物件
+         * 參數二 需要顯示文字(帶入可以是字串或是可以用到資源庫(RES)的Values的表示ex:R.string.go1)
+         * 參數三 顯示時間長度
+         * Toast.LENGTH_LONG  長時間顯示
+         * Toast.LENGTH_SHORT 短時間顯示
+         * PS 記得要下.SHOW()才能正常顯示
+         */
 
         //edittext.setText(""); // 設定指定文字
+
+        /*
+        //建立提示訊息框
+        AlertDialog builder = new AlertDialog.Builder(MainActivity.this).create();
+        builder.setTitle("訊息顯示");
+        builder.setMessage(test);
+        builder.setButton(AlertDialog.BUTTON_POSITIVE, "Button 1 Text", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int id) {
+
+                //...
+
+            } });
+
+        builder.setButton(AlertDialog.BUTTON_NEGATIVE, "Button 2 Text", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int id) {
+
+                //...
+
+            }
+        });
+
+        builder.setButton(AlertDialog.BUTTON_NEUTRAL, "Button 3 Text", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int id) {
+
+                //...
+
+            }
+        });
+
+
+        builder.show();
+        //AlertDialog dialog = builder.create();
+        */
+/*
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("訊息顯示");
+        builder.setMessage(test);
+
+        builder.setNegativeButton(R.string.button_stop , new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNeutralButton(R.string.button_close , new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setPositiveButton(R.string.button_ok , new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+
+            }
+        });
+
+        builder.show();
+
+*/
+    }
+
+    public void putcheckbok(View view){
+
+        if(hideCheckBox.isChecked()){
+            Toast.makeText(this,"Checked已打勾",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Checked打勾已取消",Toast.LENGTH_LONG).show();
+        }
+
+        editor.putBoolean("hideCheckBox",hideCheckBox.isChecked());
+        editor.commit();
+
     }
 
 }
---
+++ SimeplUi/app/src/main/java/com/example/user/simeplui/Utils.java
@@ -0,0 +1,72 @@
+package com.example.user.simeplui;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by jerry on 2015/12/1.
+ */
+public class Utils {
+
+
+    /**
+     * 將字串寫入到檔案中
+     * @param context
+     * @param fileName
+     * @param content
+     */
+    public static void writeFile(Context context , String fileName , String content)  {
+
+        try {
+            FileOutputStream fos =
+                    context.openFileOutput(fileName,context.MODE_APPEND);
+                    /*
+                     * 第一參數 表示 要給定檔案名稱
+                     * 第二參數 指定檔案寫入時可能操作模式，有四種模式
+                     * Context.MODE_PRIVATE 為預設操作模式，代表該檔是私有資料，只能被應用本身訪問，在該模式下，寫入的內容會覆蓋原檔的內容，如果想把新寫入的內容追加到原檔中請使用Context.MODE_APPEND
+                     * Context.MODE_APPEND 模式會檢查檔是否存在，存在就往檔追加內容，否則就創建新檔。
+                     * Context.MODE_WORLD_READABLE 用來控制其他應用是否有許可權讀寫該檔。
+                     * Context.MODE_WORLD_WRITEABLE  用來控制其他應用是否有許可權讀寫該檔。
+                     */
+            fos.write(content.getBytes());
+            fos.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 從檔案中讀取字串
+     * @param context
+     * @param fileName
+     * @return
+     */
+    public static String readFile(Context context , String fileName){
+
+        try {
+
+            byte[] buffer = new byte[1204];
+            FileInputStream fis = context.openFileInput(fileName);
+            fis.read(buffer,0,buffer.length);
+            fis.close();
+            return new String(buffer);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+}
